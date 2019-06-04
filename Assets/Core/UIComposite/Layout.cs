8439745bfe7c7c0bb4d450a840ea7b01140de8be
huqiang <huqiang1990@outlook.com>
2019-06-04 08:42:09 +0800

    fix

--- Assets/Core/UIComposite/Layout.cs
+++ Assets/Core/UIComposite/Layout.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 
 namespace huqiang.UIComposite
 {
-    public class LayotLine
+    public class LayoutLine
     {
         public enum Direction
         {
@@ -18,7 +18,7 @@ namespace huqiang.UIComposite
         Layout layout;
         EventCallBack callBack;
         public Direction direction { get; private set; }
-        public LayotLine(Layout lay,ModelElement mod,Direction dir,bool fix=false)
+        public LayoutLine(Layout lay,ModelElement mod,Direction dir,bool fix=false)
         {
             layout = lay;
             layout.lines.Add(this);
@@ -30,6 +30,7 @@ namespace huqiang.UIComposite
                 callBack.DragEnd = Drag;
                 direction = dir;
             }
+            mod.SetParent(layout.LineLevel);
         }
         void Drag(EventCallBack callBack,UserAction action,Vector2 v)
         {
@@ -147,22 +148,30 @@ namespace huqiang.UIComposite
             Center,Left,Top,Right,Down
         }
         public ModelElement model;
+        Layout layout;
+        public LayoutArea(Layout lay,ModelElement mod)
+        {
+            layout = lay;
+            model = mod;
+            mod.SetParent(layout.AreaLevel);
+            layout.areas.Add(this);
+        }
         /// <summary>
         /// 左边相邻的线
         /// </summary>
-        public LayotLine Left;
+        public LayoutLine Left;
         /// <summary>
         /// 右边相邻的线
         /// </summary>
-        public LayotLine Right;
+        public LayoutLine Right;
         /// <summary>
         /// 顶部相邻的线
         /// </summary>
-        public LayotLine Top;
+        public LayoutLine Top;
         /// <summary>
         /// 底部相邻的线
         /// </summary>
-        public LayotLine Down;
+        public LayoutLine Down;
         public void SizeChanged()
         {
             float rx = Right.model.data.localPosition.x;
@@ -205,7 +214,33 @@ namespace huqiang.UIComposite
         }
         void AddLeftArea()
         {
+            ModelElement m = new ModelElement();
+            m.Load(layout.AreaMod.ModData);
+            LayoutArea area = new LayoutArea(layout,m);
+            layout.areas.Add(area);
+            Vector2 size = model.data.sizeDelta;
+            float px = model.data.localPosition.x;
+            float py = model.data.localPosition.y;
+            float w = size.x * 0.5f;
+            float os = w * 0.5f;
+            m.data.localPosition.x = px - os;
+            m.data.localPosition.y = py;
+            m.data.sizeDelta = new Vector2(w,size.y);
+            m.IsChanged = true;
 
+            model.data.localPosition.x = px + os;
+            model.data.sizeDelta.x = w;
+            model.IsChanged = true;
+
+            m = new ModelElement();
+            m.Load(Left.model.ModData);
+            LayoutLine line = new LayoutLine(layout,m,LayoutLine.Direction.Vertical);
+            m.data.localPosition = new Vector3(px,py,0);
+            m.IsChanged = true;
+            area.Right = line;
+            Left = line;
+
+            ModelElement.ScaleSize(model);
         }
         void AddRightArea()
         {
@@ -222,40 +257,80 @@ namespace huqiang.UIComposite
     }
     public class Layout : ModelInital
     {
-        public List<LayotLine> lines = new List<LayotLine>();
+        public static float LineWidth = 4f;
+        public List<LayoutLine> lines = new List<LayoutLine>();
         public List<LayoutArea> areas = new List<LayoutArea>();
         ModelElement model;
         /// <summary>
         /// 左边相邻的线
         /// </summary>
-        public LayotLine Left;
+        public LayoutLine Left;
         /// <summary>
         /// 右边相邻的线
         /// </summary>
-        public LayotLine Right;
+        public LayoutLine Right;
         /// <summary>
         /// 顶部相邻的线
         /// </summary>
-        public LayotLine Top;
+        public LayoutLine Top;
         /// <summary>
         /// 底部相邻的线
         /// </summary>
-        public LayotLine Down;
-        ModelElement LineMod;
+        public LayoutLine Down;
+        public ModelElement LineMod;
+        public ModelElement AreaMod;
+        public ModelElement LineLevel;
+        public ModelElement AreaLevel;
         public override void Initial(ModelElement mod)
         {
             base.Initial(mod);
+            LineLevel = mod.Find("LineLevel");
+            AreaLevel= mod.Find("AreaLevel");
             LineMod = mod.Find("Line");
+            LineMod.activeSelf = false;
+            AreaMod = mod.Find("Area");
+            AreaMod.activeSelf = false;
             model.SizeChanged = SizeChanged;
+            InitialFixLine();
+            InitialArea();
+        }
+        void InitialFixLine()
+        {
+            Vector2 size = model.data.sizeDelta;
+            float rx = size.x*0.5f;
+            float ty = size.y;
+
+            ModelElement m = new ModelElement();
+            m.Load(LineMod.ModData);
+            Left = new LayoutLine(this,m,LayoutLine.Direction.Vertical,true);
+            Left.SetSize(new Vector2(-rx,0),new Vector2(LineWidth,size.y));
+
+            m = new ModelElement();
+            m.Load(LineMod.ModData);
+            Right = new LayoutLine(this, m, LayoutLine.Direction.Vertical, true);
+            Right.SetSize(new Vector2(rx, 0), new Vector2(LineWidth, size.y));
+
+            m = new ModelElement();
+            m.Load(LineMod.ModData);
+            Top = new LayoutLine(this, m, LayoutLine.Direction.Vertical, true);
+            Top.SetSize(new Vector2(0, ty), new Vector2(size.x, LineWidth));
+
+            m = new ModelElement();
+            m.Load(LineMod.ModData);
+            Down = new LayoutLine(this, m, LayoutLine.Direction.Vertical, true);
+            Down.SetSize(new Vector2(0, -ty), new Vector2(size.x, LineWidth));
+        }
+        void InitialArea()
+        {
+            ModelElement m = new ModelElement();
+            m.Load(AreaMod.ModData);
+            LayoutArea area = new LayoutArea(this,m);
+            area.Left = Left;
+            area.Right = Right;
+            area.Top = Top;
+            area.Down = Down;
         }
         void SizeChanged(ModelElement mod)
-        {
-
-        }
-        /// <summary>
-        /// 添加一个区域
-        /// </summary>
-        public void AddArea()
         {
 
         }
