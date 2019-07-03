3cbbbc877c4278af07b4ef2396d90b34a5fce727
huqiang <huqiang1990@outlook.com>
2019-07-03 11:31:01 +0800

    fix

--- Assets/Core/UIHelper/CreateTestHelper.cs
+++ Assets/Core/UIHelper/CreateTestHelper.cs
@@ -1,9 +1,40 @@
 ﻿using huqiang;
+using huqiang.Data;
+using huqiang.UI;
 using huqiang.UIEvent;
+using System.IO;
 using UnityEngine;
 
-public class CreateTestHelper:TestHelper
+public class CreateTestHelper: UICompositeHelp
 {
+    
+    public virtual void LoadBundle()
+    {
+#if UNITY_EDITOR
+        if (ElementAsset.bundles.Count == 0)
+        {
+            //var dic = Application.dataPath + "/StreamingAssets";
+            var dic = Application.streamingAssetsPath;
+            if (Directory.Exists(dic))
+            {
+                var bs = Directory.GetFiles(dic, "*.unity3d");
+                for (int i = 0; i < bs.Length; i++)
+                {
+                    ElementAsset.bundles.Add(AssetBundle.LoadFromFile(bs[i]));
+                }
+            }
+        }
+#endif
+    }
+    public void Initital()
+    {
+       LoadBundle();
+       TestHelper.InitialUI();
+        var c = transform.childCount;
+        for (int i = 0; i < c; i++)
+            GameObject.Destroy(transform.GetChild(i).gameObject);
+        App.Initial(transform);
+    }
     public void Build()
     {
         if(!Application.isPlaying)
@@ -28,5 +59,11 @@ public class CreateTestHelper:TestHelper
     public virtual void CreateTestPage()
     {
 
+    }
+    private void OnDestroy()
+    {
+        App.Dispose();
+        AssetBundle.UnloadAllAssetBundles(true);
+        ElementAsset.bundles.Clear();
     }
 }
