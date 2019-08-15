54311ea55b0774135d6f0a75ee99a4165def24d9
huqiang <huqiang1990@outlook.com>
2019-08-15 11:31:14 +0800

    fix

--- Assets/Core/UIHelper/AniRecordHelper.cs
+++ Assets/Core/UIHelper/AniRecordHelper.cs
@@ -3,8 +3,6 @@ using huqiang.Data;
 using huqiang.UI;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UnityEngine;
 
 public class AniRecordHelper : UICompositeHelp
