014678f68bcad6e80a6327e4b1bc99fe03da4ff3
huqiang <huqiang1990@outlook.com>
2019-09-10 14:55:01 +0800

    fix

--- Assets/Core/Gif/GifDecoder.cs
+++ Assets/Core/Gif/GifDecoder.cs
@@ -196,25 +196,25 @@ public class GifDecoder
 
             Color[] colorTabel = imageData.imageDescriptor.localColorTableFlag ? imageData.imageDescriptor.localColorTable : gifData.globalColorTable;
             // Determine base pixels
-            if (i == 0)
-            {
-                texture.SetPixels(transparentFrame);
-            }
-            else
-            {
-                if (disposalMethod == 1)
-                {
-                    texture.SetPixels(previousFrame);
-                }
-                else if (disposalMethod == 2)
-                {
-                    texture.SetPixels(transparentFrame);
-                }
-                else if (disposalMethod == 3)
-                {
-                    throw new NotImplementedException("Disposal method 3 is not implemented.");
-                }
-            }
+            //if (i == 0)
+            //{
+            //    texture.SetPixels(transparentFrame);
+            //}
+            //else
+            //{
+            //    if (disposalMethod == 1)
+            //    {
+            //        texture.SetPixels(previousFrame);
+            //    }
+            //    else if (disposalMethod == 2)
+            //    {
+            //        texture.SetPixels(transparentFrame);
+            //    }
+            //    else if (disposalMethod == 3)
+            //    {
+            //        throw new NotImplementedException("Disposal method 3 is not implemented.");
+            //    }
+            //}
 
             // Set pixels from image data
             for (int j = 0; j < imageDescriptor.imageWidth; j++)
@@ -248,7 +248,76 @@ public class GifDecoder
         }
         return sprites;
     }
+    static void CalculColors(GifData gifData)
+    {
+        Color[] previousFrame = new Color[gifData.canvasWidth * gifData.canvasHeight];
+        Color[] currentFrame = new Color[gifData.canvasWidth * gifData.canvasHeight];
+        Color[] transparentFrame = new Color[gifData.canvasWidth * gifData.canvasHeight];
 
+        // Create sprites
+        for (int i = 0; i < gifData.graphicsControlExtensions.Count; i++)
+        {
+            GifGraphicsControlExtension graphicsControlExt = gifData.graphicsControlExtensions[i];
+            GifImageDescriptor imageDescriptor = graphicsControlExt.imageDescriptor;
+            GifImageData imageData = imageDescriptor.imageData;
+            int top = imageDescriptor.imageTop;
+            int left = imageDescriptor.imageLeft;
+            int disposalMethod = graphicsControlExt.disposalMethod;
+
+            int transparencyIndex = graphicsControlExt.transparentColorFlag ? graphicsControlExt.transparentColorIndex : -1;
+            Color[] colorTabel = imageData.imageDescriptor.localColorTableFlag ? imageData.imageDescriptor.localColorTable : gifData.globalColorTable;
+
+            for (int j = 0; j < imageDescriptor.imageWidth; j++)
+            {
+                for (int k = 0; k < imageDescriptor.imageHeight; k++)
+                {
+                    int x = left + j;
+                    int y = (gifData.canvasHeight - 1) - (top + k);
+                    int colorIndex = imageData.colorIndices[j + k * imageDescriptor.imageWidth];
+                    int pixelOffset = x + y * gifData.canvasWidth;
+
+                    if (colorIndex != transparencyIndex)
+                    {
+                        currentFrame[pixelOffset] = colorTabel[colorIndex];//imageData.getColor(colorIndex);
+                    }
+                }
+            }
+            // Set texture pixels and create sprite
+           
+            // Store current frame as previous before continuing, and reset current frame
+            currentFrame.CopyTo(previousFrame, 0);
+            if (disposalMethod == 0 || disposalMethod == 2)
+            {
+                currentFrame = new Color[currentFrame.Length];
+                imageData.colors = currentFrame;
+            }
+            else
+            {
+                imageData.colors = new Color[currentFrame.Length];
+                currentFrame.CopyTo(imageData.colors,0);
+            }
+        }
+    }
+    static List<Texture2D> CreateAnimator(GifData gifData)
+    {
+        List<Texture2D> sprites = new List<Texture2D>();
+        // Create sprites
+        for (int i = 0; i < gifData.graphicsControlExtensions.Count; i++)
+        {
+            GifGraphicsControlExtension graphicsControlExt = gifData.graphicsControlExtensions[i];
+            GifImageDescriptor imageDescriptor = graphicsControlExt.imageDescriptor;
+            GifImageData imageData = imageDescriptor.imageData;
+
+            Texture2D texture = new Texture2D(gifData.canvasWidth, gifData.canvasHeight);
+    
+            // Set texture pixels and create sprite
+            texture.SetPixels(imageData.colors);
+            texture.Apply();
+            texture.filterMode = FilterMode.Point;
+            sprites.Add(texture);
+        }
+        return sprites;
+    }
     public class Mission
     {
         public string tag;
@@ -276,12 +345,13 @@ public class GifDecoder
     {
         Mission m = mis as Mission;
         m.gifdata = parseGifData(m.dat);
+        CalculColors(m.gifdata);
         ThreadMission.InvokeToMain(DataToTexture,m);
     }
     static void DataToTexture(object mis)
     {
         Mission m = mis as Mission;
-        m.texture2Ds= createAnimator(m.gifdata);
+        m.texture2Ds= CreateAnimator(m.gifdata);
         if (m.CallBack != null)
             m.CallBack(m);
     }
