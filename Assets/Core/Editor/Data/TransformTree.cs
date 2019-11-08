1a9a8cd46b5b0543cd225d1ca71acba3b26be4e8
huqiang <huqiang1990@outlook.com>
2019-11-08 11:02:18 +0800

    new data

---
+++ Assets/Core/Editor/Data/TransformTree.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using huqiang.Data;
+
+public class TransformTree
+{
+    unsafe struct LocalCoordinates
+    {
+        public Vector3 Postion;
+        public Quaternion Rotate;
+        public Vector3 Scale;
+        public Int32 Name;
+        public Int32 Child;
+        public static int Size = sizeof(LocalCoordinates);
+        public static int ElementSize = Size / 4;
+    }
+    public static unsafe byte[] Save(Transform root)
+    {
+        DataBuffer db = new DataBuffer(1024);
+        FakeStruct fake = new FakeStruct(db, LocalCoordinates.ElementSize);
+        LocalCoordinates* lc = (LocalCoordinates*)fake.ip;
+        lc->Postion = root.localPosition;
+        lc->Rotate = root.localRotation;
+        lc->Scale = root.localScale;
+        lc->Name = db.AddData(root.name);
+        lc->Child = db.AddData(SaveChild(root, db));
+        db.fakeStruct = fake;
+        return db.ToBytes();
+    }
+    static unsafe FakeStructArray SaveChild(Transform root, DataBuffer db)
+    {
+        int c = root.childCount;
+        if (c > 0)
+        {
+            FakeStructArray array = new FakeStructArray(db, LocalCoordinates.ElementSize, c);
+            Int32[] cc = new int[c];
+            for (int i = 0; i < c; i++)
+            {
+                var a = root.GetChild(i);
+                LocalCoordinates* lc = (LocalCoordinates*)array[i];
+                lc->Postion = a.localPosition;
+                lc->Rotate = a.localRotation;
+                lc->Scale = a.localScale;
+                lc->Name = db.AddData(a.name);
+                lc->Child = db.AddData(SaveChild(a, db));
+            }
+            return array;
+        }
+        return null;
+    }
+    public static unsafe void Load(Transform root, byte[] data)
+    {
+        DataBuffer db = new DataBuffer(data);
+        FakeStruct fake = db.fakeStruct;
+        LocalCoordinates* lc = (LocalCoordinates*)fake.ip;
+        root.localPosition = lc->Postion;
+        root.localRotation = lc->Rotate;
+        root.localScale = lc->Scale;
+        var array = db.GetData(lc->Child) as FakeStructArray;
+        if (array != null)
+        {
+            LoadChild(root, db, array);
+        }
+    }
+    unsafe static void LoadChild(Transform root, DataBuffer db, FakeStructArray array)
+    {
+        var c = root.childCount;
+        for (int i = 0; i < c; i++)
+        {
+            var t = root.GetChild(c);
+            var name = t.name;
+            int l = array.Length;
+            for (int j = 0; j < l; j++)
+            {
+                LocalCoordinates* lc = (LocalCoordinates*)array[j];
+                string str = db.GetData(lc->Name) as string;
+                if (str == name)
+                {
+                    t.localPosition = lc->Postion;
+                    t.localRotation = lc->Rotate;
+                    t.localScale = lc->Scale;
+                    var arr = db.GetData(lc->Child) as FakeStructArray;
+                    if (arr != null)
+                    {
+                        LoadChild(t, db, arr);
+                    }
+                }
+            }
+        }
+    }
+}
--- Assets/Core/Editor/MenuExtand.cs
+++ Assets/Core/Editor/MenuExtand.cs
@@ -8,19 +8,183 @@ using UnityEngine;
 
 public static class MenuExtand
 {
-    [MenuItem("GameObject/CreateMeshData", priority = 0)]
+    [MenuItem("GameObject/Mesh/CreateMeshData", priority = 0)]
     static void CreateMeshData()
     {
         var selected = Selection.activeObject;
         if (selected is GameObject)
         {
             string o_path = EditorUtility.SaveFilePanel("Save Resource", "Assets", selected.name, "bin");
+            if (o_path == null | o_path == "")
+                return;
             var mesh = MeshData.LoadFromGameObject((selected as GameObject).transform);
             if (File.Exists(o_path))
                 File.Delete(o_path);
             var fs = File.Create(o_path);
             mesh.WriteToStream(fs);
             fs.Dispose();
+        }
+    }
+    [MenuItem("GameObject/Mesh/ReverseMeshData", priority = 1)]
+    static void ReverseMeshData()
+    {
+        var selected = Selection.activeObject;
+        var go = selected as GameObject;
+        if (go != null)
+        {
+            var mf = go.GetComponent<MeshFilter>();
+            if (mf != null)
+            {
+                int[] tri = mf.sharedMesh.triangles;
+                int len = tri.Length / 3;
+                for (int i = 0; i < len; i++)
+                {
+                    int index = i * 3;
+                    int a = tri[index];
+                    int c = tri[index + 1];
+                    int b = tri[index + 2];
+                    tri[index + 1] = b;
+                    tri[index + 2] = c;
+                }
+                mf.sharedMesh.triangles = tri;
+                var nor = mf.sharedMesh.normals;
+                for (int i = 0; i < nor.Length; i++)
+                    nor[i] = -nor[i];
+                mf.sharedMesh.normals = nor;
+            }
+        }
+    }
+    [MenuItem("GameObject/Mesh/PositionCorrection", priority = 2)]
+    static void PositionCorrection()
+    {
+        var go = Selection.activeObject as GameObject;
+        if (go != null)
+        {
+            float lx = 0;
+            float rx = 0;
+            float ty = 0;
+            float dy = 0;
+            float fz = 0;
+            float bz = 0;
+            var t = go.transform;
+            var c = t.childCount;
+            if (c > 1)
+            {
+                var s = t.GetChild(0);
+                lx = rx = s.localPosition.x;
+                ty = dy = s.localPosition.y;
+                fz = bz = s.localPosition.z;
+                for (int i = 1; i < c; i++)
+                {
+                    var o = t.GetChild(i);
+                    var p = o.localPosition;
+                    if (p.x < lx)
+                        lx = p.x;
+                    else if (p.x > rx)
+                        rx = p.x;
+                    if (p.y < dy)
+                        dy = p.y;
+                    else if (p.y > ty)
+                        ty = p.y;
+                    if (p.z > fz)
+                        fz = p.z;
+                    else if (p.z < bz)
+                        bz = p.z;
+                }
+            }
+            Vector3 os = Vector3.zero;
+            os.x = (rx - lx) * 0.5f + lx;
+            os.y = (ty - dy) * 0.5f + dy;
+            os.z = (fz - bz) * 0.5f + bz;
+            for (int i = 0; i < c; i++)
+            {
+                var o = t.GetChild(i);
+                var p = o.localPosition;
+                p -= os;
+                o.localPosition = p;
+            }
+        }
+    }
+    [MenuItem("GameObject/Mesh/AngleZero", priority = 3)]
+    static void AngleZero()
+    {
+        var go = Selection.activeObject as GameObject;
+        if (go != null)
+        {
+            var t = go.transform;
+            var q = t.localRotation;
+            int c = t.childCount;
+            for (int i = 0; i < c; i++)
+            {
+                var o = t.GetChild(i);
+                o.localRotation = q * o.localRotation;
+                o.localPosition = q * o.localPosition;
+            }
+            t.localEulerAngles = Vector3.zero;
+        }
+    }
+    [MenuItem("GameObject/Mesh/HorizontalCorrection", priority = 4)]
+    static void HorizontalCorrection()
+    {
+        var gs = Selection.gameObjects;
+        if (gs != null)
+        {
+            if (gs.Length == 3)
+            {
+                Vector3[] pos = new Vector3[3];
+                for (int i = 0; i < 3; i++)
+                {
+                    var t = gs[i].transform;
+                    var p = t.position;
+                    var q = t.rotation;
+                    var s = t.localScale;
+                    var mf = t.GetComponent<MeshFilter>();
+                    if (mf != null)
+                    {
+                        var m = mf.sharedMesh;
+                        if (m != null)
+                        {
+                            var vert = m.vertices;
+                            for (int j = 0; j < vert.Length; j++)
+                            {
+                                var o = p + q * vert[j];
+                                if (o.y < pos[i].y)
+                                    pos[i] = o;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    [MenuItem("GameObject/Mesh/SaveTranformTree", priority = 5)]
+    static void SaveTranformTree()
+    {
+        var selected = Selection.activeObject;
+        if (selected is GameObject)
+        {
+            string o_path = EditorUtility.SaveFilePanel("Save Resource", "Assets", selected.name, "bin");
+            if (o_path == null | o_path == "")
+                return;
+            var mesh = TransformTree.Save((selected as GameObject).transform);
+            if (File.Exists(o_path))
+                File.Delete(o_path);
+            File.WriteAllBytes(o_path, mesh);
+        }
+    }
+    [MenuItem("GameObject/Mesh/LoadTranformTree", priority = 6)]
+    static void LoadTranformTree()
+    {
+        var selected = Selection.activeObject;
+        if (selected is GameObject)
+        {
+            string o_path = EditorUtility.OpenFilePanel("Assets", Application.dataPath, "bin");
+            if (o_path == null | o_path == "")
+                return;
+            if (File.Exists(o_path))
+            {
+                TransformTree.Load((selected as GameObject).transform,File.ReadAllBytes(o_path));
+            }
         }
     }
 }
